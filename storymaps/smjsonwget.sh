4156f577289a3338472ed0d8b1fe5be50c5cd161
N White <c2lkn.t@gmail.com>
2020-03-23 11:38:05 -0500

    updates json with capital letters because knightlab changed CSS

--- storymaps/smjsonwget.sh
+++ storymaps/smjsonwget.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-. ../../../storymapjumble.sh
-wget -O amyWingreen.json       http://uploads.knightlab.com/storymapjs/$storymapjumble/wingreen/published.json
-wget -O blackHawk.json         http://uploads.knightlab.com/storymapjs/$storymapjumble/black-hawk/published.json
-wget -O cgPearce.json          http://uploads.knightlab.com/storymapjs/$storymapjumble/pearce/published.json
-wget -O eraBellThompson.json   http://uploads.knightlab.com/storymapjs/$storymapjumble/thompson/published.json
-wget -O errettGraham.json      http://uploads.knightlab.com/storymapjs/$storymapjumble/errett-graham/published.json
-wget -O jhMagee.json           http://uploads.knightlab.com/storymapjs/$storymapjumble/magee/published.json
-wget -O josephWhitehouse.json  http://uploads.knightlab.com/storymapjs/$storymapjumble/whitehouse/published.json
-wget -O juliaNewberry.json     http://uploads.knightlab.com/storymapjs/$storymapjumble/julia-newberry/published.json
-wget -O raster.json            http://uploads.knightlab.com/storymapjs/$storymapjumble/raster/published.json
-wget -O thx.json               http://uploads.knightlab.com/storymapjs/$storymapjumble/thanksgiving/published.json
-wget -O xmas.json              http://uploads.knightlab.com/storymapjs/$storymapjumble/christmas/published.json
+wget -O amyWingreen.json       http://uploads.knightlab.com/storymapjs/bc2171fd2dc65da8b99c6c013d2dc670/wingreen/published.json
+scp amyWingreen.json newberry@web609.webfaction.com:webapps/timemachine/amyWingreen/storymaps/
+wget -O blackHawk.json         http://uploads.knightlab.com/storymapjs/bc2171fd2dc65da8b99c6c013d2dc670/black-hawk/published.json
+scp blackHawk.json newberry@web609.webfaction.com:webapps/timemachine/blackHawk/storymaps/
+wget -O cgPearce.json          http://uploads.knightlab.com/storymapjs/bc2171fd2dc65da8b99c6c013d2dc670/pearce/published.json
+scp cgPearce.json newberry@web609.webfaction.com:webapps/timemachine/cgPearce/storymaps/
+wget -O eraBellThompson.json   http://uploads.knightlab.com/storymapjs/bc2171fd2dc65da8b99c6c013d2dc670/thompson/published.json
+scp eraBellThompson.json newberry@web609.webfaction.com:webapps/timemachine/eraBellThompson/storymaps/
+wget -O errettGraham.json      http://uploads.knightlab.com/storymapjs/bc2171fd2dc65da8b99c6c013d2dc670/errett-graham/published.json
+scp errettGraham.json newberry@web609.webfaction.com:webapps/timemachine/errettGraham/storymaps/
+wget -O jhMagee.json           http://uploads.knightlab.com/storymapjs/bc2171fd2dc65da8b99c6c013d2dc670/magee/published.json
+scp jhMagee.json newberry@web609.webfaction.com:webapps/timemachine/jhMagee/storymaps/
+wget -O josephWhitehouse.json  http://uploads.knightlab.com/storymapjs/bc2171fd2dc65da8b99c6c013d2dc670/whitehouse/published.json
+scp josephWhitehouse.json newberry@web609.webfaction.com:webapps/timemachine/josephWhitehouse/storymaps/
+wget -O juliaNewberry.json     http://uploads.knightlab.com/storymapjs/bc2171fd2dc65da8b99c6c013d2dc670/julia-newberry/published.json
+scp juliaNewberry.json newberry@web609.webfaction.com:webapps/timemachine/juliaNewberry/storymaps/
+wget -O raster.json            http://uploads.knightlab.com/storymapjs/bc2171fd2dc65da8b99c6c013d2dc670/raster/published.json
+scp raster.json newberry@web609.webfaction.com:webapps/timemachine/raster/storymaps/
+wget -O thx.json               http://uploads.knightlab.com/storymapjs/bc2171fd2dc65da8b99c6c013d2dc670/thanksgiving/published.json
+scp thx.json newberry@web609.webfaction.com:webapps/timemachine/thx/storymaps/
+wget -O xmas.json              http://uploads.knightlab.com/storymapjs/bc2171fd2dc65da8b99c6c013d2dc670/christmas/published.json
+scp xmas.json newberry@web609.webfaction.com:webapps/timemachine/xmas/storymaps/
